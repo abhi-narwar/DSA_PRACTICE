9a268a9d8e033ef8ec278ec9494899bebe74b112
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-11-03 08:40:35 +0530

    added code

---
+++ Leetcode Questions/jump_game.java
@@ -0,0 +1,12 @@
+class jump_game {
+    public boolean canJump(int[] nums) {
+        int farthest = 0;  
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) return false;  
+            farthest = Math.max(farthest, i + nums[i]);
+        }
+        
+        return true; 
+    }
+}
