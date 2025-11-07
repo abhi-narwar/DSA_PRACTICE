c1b799ebc34d53a721395a017ed2a68703a70a96
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-11-07 17:17:30 +0530

    added code

---
+++ Striver's DSA Sheet Problems/Arrays/rotate_array_by_d_steps.java
@@ -0,0 +1,26 @@
+class rotate_array_by_d_steps {
+    
+    void rotateArr(int arr[], int d) {
+        int n = arr.length;
+        d = d % n; 
+        
+        reverse(arr, 0, d - 1);
+        
+        
+        reverse(arr, d, n - 1);
+        
+  
+        reverse(arr, 0, n - 1);
+    }
+    
+  
+    void reverse(int arr[], int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
