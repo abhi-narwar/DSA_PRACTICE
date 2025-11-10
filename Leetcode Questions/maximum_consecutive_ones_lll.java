48ea91e8a57ba48658186fcaa84a1d62253e6e2f
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-11-10 13:36:34 +0530

    added code

---
+++ Leetcode Questions/maximum_consecutive_ones_lll.java
@@ -0,0 +1,27 @@
+class maximum_consecutive_ones_lll {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;  
+        int right;     
+        int zeros = 0;
+        int maxLen = 0;
+
+        for (right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeros++;
+            }
+
+            
+            while (zeros > k) {
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+
+            
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
