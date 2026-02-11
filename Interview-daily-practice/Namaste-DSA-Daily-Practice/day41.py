f8ed1b83f6a785cf8fbed29c187a1378cb5b5736
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2026-02-11 12:32:18 +0530

    added code

---
+++ Interview-daily-practice/Namaste-DSA-Daily-Practice/day41.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+
+        for i in range(len(s)):
+            char_s = s[i]
+            char_t = t[i]
+
+            # Check mapping from s to t
+            if char_s in s_to_t:
+                if s_to_t[char_s] != char_t:
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+
+            # Check mapping from t to s
+            if char_t in t_to_s:
+                if t_to_s[char_t] != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+
+        return True
